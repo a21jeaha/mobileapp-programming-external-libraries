fb957aae8c3a52e1062f544a0db1ca5639aaf663
Jean-Paul <a21jeaha@student.his.se>
2022-05-13 09:08:49 +0200

    internet access permission added to manifest. picasso used to place image taken from an URL and placed it in a imageView

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.picasso_image);
+
+        Picasso.get().load("https://www.his.se/contentassets/df34584991724cd3be379489759a3d7c/_hor98703.jpg").resize(200, 200).into(imageView);
     }
 }
